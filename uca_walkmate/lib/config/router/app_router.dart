80e33b4ea9e0e2e6614fbaf22f51eee53853999f
FR0K3 <00174320@uca.edu.sv>
2024-07-05 20:37:13 -0600

    feat: Check auth in router

--- uca_walkmate/lib/config/router/app_router.dart
+++ uca_walkmate/lib/config/router/app_router.dart
@@ -1,16 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:uca_walkmate/config/router/app_router_notifier.dart';
+import 'package:uca_walkmate/presentation/providers/auth_provider.dart';
 import 'package:uca_walkmate/presentation/screens/auth/check_auth_status_screen.dart';
 import 'package:uca_walkmate/presentation/screens/event/event_screen.dart';
 import 'package:uca_walkmate/presentation/screens/home/home_screen.dart';
 import 'package:uca_walkmate/presentation/screens/login/login_screen.dart';
 import 'package:uca_walkmate/presentation/screens/signup/sign_up_screen.dart';
 import 'package:uca_walkmate/presentation/screens/subject/subject_screen.dart';
-import 'package:uca_walkmate/presentation/screens/subject_form_screen.dart';
 
 final goRouterProvider = Provider((ref) {
+  final goRouterNotifier = ref.read(goRouterNotifierProvider);
+
   return GoRouter(
     initialLocation: '/check-auth-status',
+    refreshListenable: goRouterNotifier,
     routes: [
       GoRoute(
         name: CheckAuthStatusScreen.routeName,
@@ -37,6 +41,32 @@ final goRouterProvider = Provider((ref) {
         path: '/signup',
         builder: (context, state) => const SignUpScreen(),
       ),
+      GoRoute(
+        name: HomeScreen.routeName,
+        path: '/',
+        builder: (context, state) => const HomeScreen(),
+      )
     ],
+    redirect: (context, state) {
+      final isGoingTo = state.matchedLocation;
+      final authStatus = goRouterNotifier.authStatus;
+
+      print('AuthStatus: $authStatus, isGoingTo: $isGoingTo');
+      
+      if (isGoingTo == '/check-auth-status' && authStatus == AuthStatus.checking) return null;
+
+      if (authStatus == AuthStatus.unathenticated) {
+        if (isGoingTo == '/login' || isGoingTo == '/signup') return null;
+
+        return '/login';
+      }
+
+      if (authStatus == AuthStatus.authenticated) {
+        if (isGoingTo == '/login' || isGoingTo == '/signup' || isGoingTo == '/check-auth-status') return '/';
+      }
+
+
+      return null;
+    }
   );
 });
