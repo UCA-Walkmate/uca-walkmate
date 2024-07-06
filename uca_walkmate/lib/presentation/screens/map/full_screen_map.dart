5533a358daf4aaf672ad66b511d85772ffaf8515
TitoCarpio <00226620@uca.edu.sv>
2024-07-06 00:08:58 -0600

    :heavy_plus_sign: add boundaries to the map view

--- uca_walkmate/lib/presentation/screens/map/full_screen_map.dart
+++ uca_walkmate/lib/presentation/screens/map/full_screen_map.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:uca_walkmate/config/constants/environment.dart';
 import 'package:uca_walkmate/data/apis/graph_hopper_api.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class FullScreenMap extends StatefulWidget {
-  static const String  routeName = 'home';
+  static const String routeName = 'home';
   const FullScreenMap({super.key});
 
   @override
@@ -15,7 +16,7 @@ class FullScreenMap extends StatefulWidget {
 class _FullScreenMapState extends State<FullScreenMap> {
   //llave de la api
   final GraphHopperService graphHopperService =
-      GraphHopperService(apiKey: 'ba1dcdf2-69f9-4138-9fcc-d57aa2476db0');
+      GraphHopperService(apiKey: Environment.apiKey);
 
   //lista de puntos de la ruta para mapearlos
   List<LatLng> route = [];
@@ -23,8 +24,8 @@ class _FullScreenMapState extends State<FullScreenMap> {
   //obteniendo los puntos de la ruta llamando los servicios de la api
   getCoordinate() async {
     //punto de inicio y fin de la ruta
-    LatLng start = const LatLng(13.679709, -89.236811);
-    LatLng end = const LatLng(13.67983, -89.23553);
+    LatLng start = const LatLng(-89.2213012319936, 13.692324732823067);
+    LatLng end = const LatLng(-89.25114786256749, 13.669090706918311);
 
     //obteniendo los puntos de la ruta
     List<LatLng> puntos = await graphHopperService.getRoute(start, end);
@@ -36,93 +37,65 @@ class _FullScreenMapState extends State<FullScreenMap> {
     });
   }
 
-  // int _selectedIndex = 0;
-
-  // void _onBottomBarItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //     // Maneja la lógica adicional aquí si es necesario
-      
-  //   });
-  // }
-
+  // defininiendo los limites del mapa
+  final boundsss = LatLngBounds(
+      const LatLng(13.691159, -89.222943), const LatLng(13.668918, -89.251077));
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: [
-          FlutterMap(
-            options: const MapOptions(
-              initialCenter: LatLng(13.680144, -89.236275),
+      children: [
+        FlutterMap(
+          options: MapOptions(
+              initialCenter: const LatLng(13.680144, -89.236275),
               initialZoom: 15,
               minZoom: 10,
+              cameraConstraint: CameraConstraint.contain(bounds: boundsss)),
+          children: [
+            TileLayer(
+              urlTemplate: Environment.mapBox,
+              userAgentPackageName: 'com.example.mapa_uca',
+              retinaMode: true,
             ),
-            children: [
-              TileLayer(
-                urlTemplate:
-                    'https://api.mapbox.com/styles/v1/titocarpio/clxflk0ox00qd01qm0d8rf6hu/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGl0b2NhcnBpbyIsImEiOiJjbHg5dDB3NjQycWcwMmtxMTB3YmJtMnpyIn0.DaW6faDhPKXABvltNU8vig',
-                userAgentPackageName: 'com.example.mapa_uca',
-                retinaMode: true,
-              ),
-              RichAttributionWidget(
-                attributions: [
-                  TextSourceAttribution(
-                    '© Mapbox',
-                    onTap: () => launchUrl(
-                        Uri.parse('https://www.mapbox.com/about/maps/')),
-                  ),
-                  TextSourceAttribution(
-                    '© OpenStreetMap contributors',
-                    onTap: () => launchUrl(
-                        Uri.parse('https://openstreetmap.org/copyright')),
-                  ),
-                  TextSourceAttribution(
-                    'Improve this map',
-                    onTap: () => launchUrl(
-                        Uri.parse('https://www.mapbox.com/map-feedback/')),
-                  ),
-                ],
-              ),
-              PolylineLayer(
-                // polylineCulling: false,
-                polylines: [
-                  Polyline(
-                    points: route,
-                    color: Colors.blue,
-                    strokeWidth: 4,
-                  ),
-                ],
-              ),
-            ],
-          ),
-         FloatingActionButton(
-        
-        backgroundColor: Colors.blueAccent,
-        onPressed: () => getCoordinate(),
-        child: const Icon(
-          Icons.route,
-          color: Colors.red,
+            RichAttributionWidget(
+              attributions: [
+                TextSourceAttribution(
+                  '© Mapbox',
+                  onTap: () => launchUrl(
+                      Uri.parse('https://www.mapbox.com/about/maps/')),
+                ),
+                TextSourceAttribution(
+                  '© OpenStreetMap contributors',
+                  onTap: () => launchUrl(
+                      Uri.parse('https://openstreetmap.org/copyright')),
+                ),
+                TextSourceAttribution(
+                  'Improve this map',
+                  onTap: () => launchUrl(
+                      Uri.parse('https://www.mapbox.com/map-feedback/')),
+                ),
+              ],
+            ),
+            PolylineLayer(
+              // polylineCulling: false,
+              polylines: [
+                Polyline(
+                  points: route,
+                  color: Colors.blue,
+                  strokeWidth: 4,
+                ),
+              ],
+            ),
+          ],
         ),
-      )
-          //boton flotante para obtener la ruta de la api TODO: se puede mejorar
-          // Positioned(
-          //   bottom: 20,
-          //   //posicionando el widget en el centro pero dependiendo del tamaño de la pantalla
-          //   left: 30,
-          //   right:  30,
-          //   child: Center(
-          //     child: CustomBottomAppBar(
-          //       // items: [
-          //       //   BottomBarItem(icon: Icons.location_on_outlined),
-          //       //   BottomBarItem(icon: Icons.book_outlined),
-          //       //   BottomBarItem(icon: Icons.calendar_today_rounded),
-          //       // ],
-          //       selectedIndex: _selectedIndex,
-          //       onTap: _onBottomBarItemTapped,
-          //     ),
-          //   ),
-          // ),
-        ],
-      );
-      
+        FloatingActionButton(
+          backgroundColor: Colors.blueAccent,
+          onPressed: () => getCoordinate(),
+          child: const Icon(
+            Icons.route,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
   }
 }
