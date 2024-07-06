d763eefec90e0fab68dac09f4c46ebd173ce4a26
FR0K3 <00174320@uca.edu.sv>
2024-07-05 21:46:24 -0600

    refactor: Rename subjects screen and converted into Stateless Widget

--- uca_walkmate/lib/presentation/screens/home/home_screen.dart
+++ uca_walkmate/lib/presentation/screens/home/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uca_walkmate/presentation/providers/index_provider.dart';
 import 'package:uca_walkmate/presentation/screens/event/event_screen.dart';
 import 'package:uca_walkmate/presentation/screens/map/full_screen_map.dart';
-import 'package:uca_walkmate/presentation/screens/subject/subject_screen.dart';
+import 'package:uca_walkmate/presentation/screens/subject/subjects_screen.dart';
 
 class HomeScreen extends ConsumerWidget {
   static const String routeName = 'home-screen';
@@ -14,7 +14,7 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
      final screens = [
       const FullScreenMap(),
-      const SubjectScreen(),
+      const SubjectsScreen(),
       const EventScreen(),
     ];
     final selectedIndex = ref.watch(selectedIndexProvider);
--- uca_walkmate/lib/presentation/screens/subject/subjects_screen.dart
+++ uca_walkmate/lib/presentation/screens/subject/subjects_screen.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
-class SubjectScreen extends StatefulWidget {
+class SubjectsScreen extends StatelessWidget {
   static const String routeName = 'subject';
-  const SubjectScreen({super.key});
+  const SubjectsScreen({super.key});
 
-  @override
-  State<SubjectScreen> createState() => _SubjectScreenState();
-}
-
-class _SubjectScreenState extends State<SubjectScreen> {
   @override
   Widget build(BuildContext context) {
     return Stack(
