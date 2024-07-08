3f2864dae97271e27355e4364d2ad714e5ebd452
FR0K3 <00174320@uca.edu.sv>
2024-07-07 22:44:45 -0600

    style: Removed unused import

--- uca_walkmate/lib/presentation/screens/home/home_screen.dart
+++ uca_walkmate/lib/presentation/screens/home/home_screen.dart
@@ -4,7 +4,6 @@ import 'package:uca_walkmate/presentation/providers/index_provider.dart';
 import 'package:uca_walkmate/presentation/screens/event/event_screen.dart';
 import 'package:uca_walkmate/presentation/screens/map/full_screen_map.dart';
 import 'package:uca_walkmate/presentation/screens/subject/subjects_screen.dart';
-import 'package:uca_walkmate/presentation/widgets/bars/searchbar/search_app_bar.dart';
 
 class HomeScreen extends ConsumerWidget {
   static const String routeName = 'home-screen';
